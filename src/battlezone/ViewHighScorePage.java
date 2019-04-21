34dbf765d3b48e6b2beadcf6ff6d42fd6f2c58be
nathanmacleod <42585178+NathanMacLeod@users.noreply.github.com>
2019-04-20 22:19:39 -0700

    Update ViewHighScorePage.java

--- src/battlezone/ViewHighScorePage.java
+++ src/battlezone/ViewHighScorePage.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * File added by Nathan MacLeod 2019
  */
 package battlezone;
 import java.awt.Font;
