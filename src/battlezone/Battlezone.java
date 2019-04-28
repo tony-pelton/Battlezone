d6911fb3f36a04ef3494c815f27c80d693d5d8e8
nathanmacleod <42585178+NathanMacLeod@users.noreply.github.com>
2019-04-28 14:52:34 -0700

    Update Battlezone.java
    Making it so files can be accessed by the jar file on its own.

--- src/battlezone/Battlezone.java
+++ src/battlezone/Battlezone.java
@@ -16,6 +16,7 @@ import java.awt.event.*;
 import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.Scanner;
+import java.io.File;
 import java.util.StringTokenizer;
 /**
  *
@@ -140,7 +141,7 @@ public class Battlezone extends JFrame implements Runnable {
         int[] nameChars = ((WriteHighScorePage)menuPages[5]).getNameChars();
         String name = "" + (char) nameChars[0] + (char) nameChars[1] + (char) nameChars[2];
         scoreList[replaceLocation] = name + score;
-        PrintWriter out = new PrintWriter(".\\src\\scores.txt");
+        PrintWriter out = new PrintWriter(new File(Class.class.getResource("/scores.txt").getFile()));
         for(int k = 0; k < scoreList.length; k++) {
             out.println(scoreList[k]);
         }
@@ -157,7 +158,7 @@ public class Battlezone extends JFrame implements Runnable {
         retrives the high scores from the text document
         */
         try {
-            Scanner wanner = new Scanner(new FileReader(".//src//scores.txt"));
+            Scanner wanner = new Scanner(Class.class.getResourceAsStream("/scores.txt"));
             String scores = "";
             while(wanner.hasNext()) {
                 scores += wanner.next() + " ";
